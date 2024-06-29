57e401a1550e0391a45478d8554621234bf4c772
mammad7527 <mammad75.fered27@gmail.com>
2024-06-29 08:40:32 +0330

    add test file

---
+++ test.py
@@ -0,0 +1,11 @@
+def get_string_length(text):
+  return len(text)
+
+# Get user input
+user_string = input("Enter a string: ")
+
+# Get the length of the string
+string_length = get_string_length(user_string)
+
+# Print the length
+print(f"The length of the string is: {string_length}")
